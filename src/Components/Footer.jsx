f9f1a18f91630cb67dfb56e3fca57ddfe282f6b5
Suleman12723 <suleman.sohail9@gmail.com>
2023-08-07 04:45:13 +0500

    Suleman: Footer and FAQs

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -14,14 +14,14 @@ const Footer = () => {
             <div className='self-center relative top-7'>
                 <KeyboardArrowUpIcon />
             </div>
-            <div className='flex flex-row justify-around p-5 relative top-16 '>
+            <div className='flex flex-col md:flex-row justify-around p-5 relative top-16 '>
                 <div className='flex-1 flex flex-col '>
                     <div className='w-[80%] self-center space-y-3'>
                         <div className='flex flex-row items-center space-x-3'>
                             <img src={MainLogo} alt="logo" />
                             <p className='font-heading text-heading'>Current <span className='font-simple'>VPN</span></p>
                         </div>
-                        <p className='font-simple text-simple w-3/4'>
+                        <p className='font-simple text-simple w-3/4 text-center md:text-start'>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim...
                         </p>
                         <div className='flex flex-row items-center space-x-3'>
@@ -32,7 +32,7 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <div className='flex-1 flex flex-row justify-around'>
+                <div className='flex-1 flex flex-col items-center text-center md:items-start md:text-start md:flex-row justify-around'>
                     <div className='flex flex-col space-y-3'>
                         <p className='font-heading text-heading'>Products</p>
                         <p className='font-simple text-simple text-[#8D98A9] hover:text-[#092857] cursor-pointer'>VPN For Mac</p>
