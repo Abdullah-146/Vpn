2b1eff48d25d7507a1bd2f0b0bd85607e3c5b791
Abdullah-146 <abdullah.mumtaz65@gmail.com>
2023-08-07 21:33:26 +0500

    comitted

--- src/Components/InputField.jsx
+++ src/Components/InputField.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 
-function InputField({ styles }) {
+function InputField({ styles, Icon, text = "Email", type }) {
   return (
     <div
-      className={`border-2 flex items-center border-black bg-white rounded-full px-5 py-6 w-full mb-5 ${styles}   `}
+      className={`border-2 flex items-center border-[#A1CDFF] bg-white rounded-full px-5 py-6  mb-5 w-full  `}
     >
-      <PersonOutlineIcon />
-      <div>
-        <input
-          type="text"
-          placeholder="Email"
-          className="w-full outline-none bg-transparent"
-        />
+      {text == "Password" ? <img src={Icon} alt="" /> : <Icon />}
+      <div className=" w-full text-start h-full ml-2">
+        <p className="text-[#9999AF] text-base">{text}</p>
+
+        <div>
+          <input
+            type={type}
+            placeholder=""
+            className="w-full outline-none bg-transparent"
+          />
+        </div>
       </div>
     </div>
   );
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import LoginSvg from "../assets/LoginSvg.svg";
 import InputField from "../Components/InputField";
+import key from "../assets/key.svg";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import DoneIcon from "@mui/icons-material/Done";
+import Button from "../Components/Button";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
   return (
-    <div className="h-screen w-screen bg-gradient-to-r from-[#c4e9f974] to-white pt-8 ">
+    <div className="h-screen w-screen bg-gradient-to-r from-[#c4e9f974] to-white pt-8 overflow-hidden"  onClick={()=> navigate(-1)} >
       {/* make button named back */}
       <div className=" flex justify-center items-center py-5 rounded-full bg-white w-[150px]  ml-10 cursor-pointer  ">
         <ArrowBackIcon /> Back
@@ -13,19 +19,50 @@ function Login() {
       {/* button out */}
       {/* main div in */}
       <div className="flex justify-between ">
-        <img src={LoginSvg} alt="" className="" />
+        <img src={LoginSvg} alt="" className="  " />
         {/* All login Fields in */}
-        <div className="flex flex-col w-full  text-center items-center  border-2 border-black  ">
+        <div className="flex flex-col w-full  text-center items-center   ">
           <h1 className="text-4xl font-medium text-simple flex">
             <span>Login In Your&nbsp;</span>
             <span className="font-heading">Account!</span>
           </h1>
-          <p className="text-xl text-[#7B899D] font-simple my-10">
+          <p className="text-xl text-[#7B899D] font-simple mt-4 mb-10">
             Please log in with scan qr code or fill out the Form...
           </p>
           {/* form */}
           <form className="flex flex-col justify-center items-center w-full">
-            <InputField type="text" placeholder="Email" styles={"w-[400px]"} />
+            <div className="w-[50%]">
+              <InputField
+                type="text"
+                placeholder="Email"
+                Icon={PersonOutlineIcon}
+              />
+              <InputField type="password" text="Password" Icon={key} />
+            </div>
+            {/* terms and conditions */}
+            <div className="flex justify-between w-[50%]">
+              <div className="flex  items-center">
+                <div className="bg-[#0A7AF9] p-[2px] rounded-full  ">
+                  <DoneIcon sx={{ color: "white", fontSize: 25 }} />
+                </div>
+                <p className="ml-2 text-[#4A5A71] font-semibold">
+                  I Agree To The Rules
+                </p>
+              </div>
+              <p className="text-[#0A7AF9] font-bold  ">Forgot Password? </p>
+            </div>
+            {/* terms and conditions out */}
+
+            <div className="w-[50%]">
+              <Button />
+            </div>
+
+            <div className="flex  items-center mt-5">
+              <p className="ml-2 text-[#A6A6BB] font-semibold">
+                Do You Have Account? &nbsp;
+              </p>
+              <p className="text-[#0A7AF9] font-bold  "> Sign Up </p>
+            </div>
           </form>
         </div>
       </div>
