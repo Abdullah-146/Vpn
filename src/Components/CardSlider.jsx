e8e9515822e22641f4704b4f54bf8c927a4998a8
Abdullah-146 <abdullah.mumtaz65@gmail.com>
2023-09-01 06:01:10 +0500

    Comitted

--- src/Components/CardSlider.jsx
+++ src/Components/CardSlider.jsx
@@ -2,27 +2,31 @@ import React, { useState } from "react";
 import NordVPN from "../assets/NordVPN.png";
 import Express from "../assets/Express.png";
 import Current from "../assets/Current.png";
+import { NavLink } from "react-router-dom";
+import verified from "../assets/verified.svg";
+import Cross from "../assets/Cross.svg";
+import { ArrowLeft, ArrowRight } from "@mui/icons-material";
 
 const cardData = [
   {
     id: 2,
     title: "Card 2",
     content: {
-      line1: "Enjoy the Stable",
-      line2: "Connection And Ping",
-      line3: "Enjoy the Stable",
-      line4: "Connection And Ping",
+      line1: { text: "Enjoy the Stable", bool: false },
+      line2: { text: "Connection And Ping", bool: true },
+      line3: { text: "Enjoy the Stable", bool: false },
+      line4: { text: "Connection And Ping", bool: true },
     },
     image: Express,
   },
   {
     id: 1,
-    title: "Card 1",
+    title: "CurrentVPN",
     content: {
-      line1: "Enjoy the Stable",
-      line2: "Connection And Ping",
-      line3: "Enjoy the Stable",
-      line4: "Connection And Ping",
+      line1: { text: "Enjoy the Stable", bool: true },
+      line2: { text: "Connection And Ping", bool: true },
+      line3: { text: "Enjoy the Stable", bool: true },
+      line4: { text: "Connection And Ping", bool: true },
     },
     image: NordVPN,
   },
@@ -30,10 +34,10 @@ const cardData = [
     id: 3,
     title: "Card 3",
     content: {
-      line1: "Enjoy the Stable",
-      line2: "Connection And Ping",
-      line3: "Enjoy the Stable",
-      line4: "Connection And Ping",
+      line1: { text: "Enjoy the Stable", bool: true },
+      line2: { text: "Connection And Ping", bool: true },
+      line3: { text: "Enjoy the Stable", bool: false },
+      line4: { text: "Connection And Ping", bool: false },
     },
     image: Express,
   },
@@ -76,47 +80,77 @@ const CardCarousel = () => {
       ];
     setCurrentCardIndex(newIndex);
   };
+  const [links, setLinks] = useState(["Home", "AboutUs", "Services", "Plans"]);
 
   return (
-    <div className="flex items-center justify-center">
-      <button
-        className="mr-2 p-2 bg-gray-300 rounded-lg"
-        onClick={handleNextClick}
-      >
-        &lt;
+    <div className="md:flex items-center  container hidden justify-center   ">
+      <button onClick={handleNextClick}>
+        <ArrowLeft
+          sx={{
+            color: "black",
+            backgroundColor: "white",
+            padding: "0rem",
+            borderRadius: "30%",
+            fontSize: "3rem",
+            ":hover": {
+              color: "white",
+              backgroundColor: "#0A7AF9",
+              borderRadius: "50%",
+              transition: "all 0.5s ease-in-out",
+            },
+          }}
+        />
       </button>
-      <div className="flex space-x-4   ">
+      <div className="flex  items-center justify-around w-[90%] lg:w-[75%]   ">
         {cardData.map((card, index) => (
           <div
             key={card.id}
-            className={`p-4 border rounded-lg ${
-              index === 1 ? "bg-blue-200" : "bg-gray-100"
+            className={`p-4 border  ${
+              index === 1 ? "bg-[#e4f6f674]" : "white"
             }  transform ${
               index === 1 ? "scale-110" : "scale-90"
-            } transition-transform ease-out duration-300 `}
+            } transition-transform ease-out duration-300  px-10 py-8 rounded-3xl  `}
             style={{
               minWidth: "200px",
               transition: "flex 0.5s ease-in-out",
             }}
           >
-            <img src={card.image} alt={card.title} className="mb-2" />
+            <img src={card.image} alt={card.title} className="mb-8" />
 
             {Object.keys(card.content).map((key, index) => {
               return (
-                <p className="text-simple font-heading my-2">
-                  {card.content[key]}
-                </p>
+                <div className="flex items-center">
+                  {card.content[key].bool ? (
+                    <img src={verified} alt="" className="mr-2" />
+                  ) : (
+                    <img src={Cross} alt="" className="mr-2" />
+                  )}
+
+                  <p className="text-simple font-simple my-1">
+                    {card.content[key].text}
+                  </p>
+                </div>
               );
             })}
           </div>
         ))}
-        <div className="border-2 border-black ">Jejvf</div>
       </div>
-      <button
-        className="ml-2 p-2 bg-gray-300 rounded-lg"
-        onClick={handlePrevClick}
-      >
-        &gt;
+
+      <button onClick={handlePrevClick}>
+        <ArrowRight
+          sx={{
+            color: "black",
+            backgroundColor: "white",
+            padding: "0rem",
+            fontSize: "3rem",
+            ":hover": {
+              color: "white",
+              backgroundColor: "#0A7AF9",
+              borderRadius: "50%",
+              transition: "all 0.5s ease-in-out",
+            },
+          }}
+        />
       </button>
     </div>
   );
