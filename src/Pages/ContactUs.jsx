38b4e93ce1c1351d36eba162d001a1f91fabe207
Abdullah-146 <abdullah.mumtaz65@gmail.com>
2024-05-09 19:35:10 +0500

    tweaks in footer

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -4,19 +4,18 @@ import SVG from "../assets/SVG1.svg";
 import icon from "../assets/icon1.svg";
 import Nav from "../Components/Nav";
 import Footer from "../Components/Footer";
-
-import chevronDown from "../assets/chevronDown.svg";
-import ContactUsComponent from "../Components/ContactUsComponent";
-import FaqIcon from "../Svgs/FaqIcon";
+import ContactUsCard from "../assets/ContactUsCard.svg";
+import Chart from "../assets/chart-column.svg";
 import ContactUsSvg from "../assets/ContactUsSvg.svg";
 import MessengerSvg from "../assets/messanger.svg";
+import ContactUsComponent from "../Components/ContactUsComponent";
+
 import Share from "../assets/Share";
 
 function ContactUs() {
   return (
     <div className="bg-gradient-to-r from-[#c4e9f974] to-white">
       <Nav />
-
       <ContactUsComponent
         SVG={ContactUsSvg}
         icon={MessengerSvg}
@@ -24,6 +23,7 @@ function ContactUs() {
         line1="Send A Message!"
         line2=" "
       />
+
       <div className="flex flex-row items-center justify-center space-x-4 mt-10">
         <Share />
         <p className="font-subHeading text-simple">Connect With Us</p>
@@ -31,9 +31,13 @@ function ContactUs() {
       <p className="font-heading text-heading text-center">Contact Us..!</p>
 
       <Card
-        SVG={SVG}
-        icon={icon}
-        content="Just as a well-crafted tea invigorates your senses, this VPN company seamlessly integrates robust privacy features with swift internet connectivity. Share your moments, connect, and explore without compromise as you bask in the comfort of a VPN that not only safeguards your digital footprint but also ensures a smooth and rapid online experience."
+        SVG={ContactUsCard}
+        reverse={false}
+        line1="Enjoy the Stable"
+        line2="Connection And Ping"
+        iconText="Safe"
+        content="Experience uninterrupted online activities as this VPN optimizes your network for a consistently smooth performance. Whether youare gaming, streaming, or engaging in video calls, relish the seamless connectivity that minimizes latency. Elevate your digital experience and bid farewell to disruptions – our VPN ensures a stable connection and minimal ping for a truly enjoyable online journey. "
+        icon={Chart}
       />
 
       <Footer />
