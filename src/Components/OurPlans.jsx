aeccb320b90af45d8aa2262af30babe6eaf4a4cc
Hassan Aftab <hassannaftabb@gmail.com>
2024-08-09 16:25:03 +0500

    chore: plans and footer update

--- src/Components/OurPlans.jsx
+++ src/Components/OurPlans.jsx
@@ -1,47 +1,47 @@
-import React from "react";
-import FreePlanA from "../assets/FreePlanA.svg";
-import ProPlanA from "../assets/ProPlanA.svg";
-import AdvancePlanA from "../assets/AdvancePlanA.svg";
-import Plan from "./Plan";
-import Save40 from "../assets/Save40.svg";
+import React from 'react';
+import FreePlanA from '../assets/FreePlanA.svg';
+import ProPlanA from '../assets/ProPlanA.svg';
+import AdvancePlanA from '../assets/AdvancePlanA.svg';
+import Plan from './Plan';
+import Save40 from '../assets/Save40.svg';
 
 function OurPlans() {
   const [toggle, setToggle] = React.useState(true);
 
   const [contentfree, setContentfree] = React.useState({
-    line1: { text: "Enjoy the Stable", bool: true },
-    line2: { text: "Devices 1", bool: true },
-    line3: { text: "Premium Server", bool: false },
-    line4: { text: "High Speed", bool: false },
-    line5: { text: "Email Support", bool: false },
-    line6: { text: "Malware Protection", bool: false },
-    line7: { text: "Gaming or Streaming Premium", bool: false },
+    line1: { text: 'Enjoy the Stable', bool: true },
+    line2: { text: 'Devices 1', bool: true },
+    line3: { text: 'Premium Server', bool: false },
+    line4: { text: 'High Speed', bool: false },
+    line5: { text: 'Email Support', bool: false },
+    line6: { text: 'Malware Protection', bool: false },
+    line7: { text: 'Gaming or Streaming Premium', bool: false },
   });
   const [contentPremimum, setContentPremimum] = React.useState({
-    line1: { text: "Enjoy the Stable", bool: true },
-    line2: { text: "Devices 1", bool: true },
-    line3: { text: "Premium Server", bool: true },
-    line4: { text: "High Speed", bool: true },
-    line5: { text: "Email Support", bool: true },
-    line6: { text: "Malware Protection", bool: true },
-    line7: { text: "Gaming or Streaming Premium", bool: true },
+    line1: { text: 'Enjoy the Stable', bool: true },
+    line2: { text: 'Devices 10', bool: true },
+    line3: { text: 'Premium Server', bool: true },
+    line4: { text: 'High Speed', bool: true },
+    line5: { text: 'Email Support', bool: true },
+    line6: { text: 'Malware Protection', bool: true },
+    line7: { text: 'Gaming or Streaming Premium', bool: true },
   });
   return (
-    <div className="container relative">
-      <div className="flex justify-center items-center w-full  ">
-        <div className="border-2 bg-gradient-to-r py-1 px-1 rounded-3xl from-[#69cce263] to-[#8ef0e83a] flex space-x-2 ml-0 md:ml-64 cursor-pointer">
+    <div className='container relative'>
+      <div className='flex justify-center items-center w-full  '>
+        <div className='border-2 bg-gradient-to-r py-1 px-1 rounded-3xl from-[#69cce263] to-[#8ef0e83a] flex space-x-2 ml-0 md:ml-64 cursor-pointer'>
           <div
             className={` py-3 rounded-3xl  px-8   `}
             style={{
               background: toggle
-                ? "linear-gradient(to right,#fff, #69cce227)"
-                : "white",
+                ? 'linear-gradient(to right,#fff, #69cce227)'
+                : 'white',
             }}
             onClick={() => setToggle(true)}
           >
             <p
               className={`text-center w-16 ${
-                toggle ? "text-[#0A7AF9]" : "text-black"
+                toggle ? 'text-[#0A7AF9]' : 'text-black'
               }`}
             >
               Monthly
@@ -51,66 +51,66 @@ function OurPlans() {
             className={` py-3 rounded-3xl  px-8 }  `}
             style={{
               background: !toggle
-                ? "linear-gradient(to right,#fff, #69cce227)"
-                : "white",
+                ? 'linear-gradient(to right,#fff, #69cce227)'
+                : 'white',
             }}
             onClick={() => setToggle(false)}
           >
             <p
               className={`text-center w-16 ${
-                !toggle ? "text-[#0A7AF9]" : "text-black"
+                !toggle ? 'text-[#0A7AF9]' : 'text-black'
               }`}
             >
               Yearly
             </p>
           </div>
         </div>
-        <div className="ml-20 hidden md:block">
-          <img src={Save40} alt="" className="" />
+        <div className='ml-20 hidden md:block'>
+          <img src={Save40} alt='' className='' />
         </div>
       </div>
 
       {/* Render Three Tables Free Plan & Pro & Advanced */}
-      <div className="flex flex-col  md:flex-row justify-center items-center my-16  ">
+      <div className='flex flex-col  md:flex-row justify-center items-center my-16  '>
         {toggle ? (
           <>
             <Plan
-              title="Free Plan"
+              title='Free Plan'
               svg={FreePlanA}
               price={0}
-              color1={"#A07CED"}
-              color2={"#5E63DA"}
+              color1={'#A07CED'}
+              color2={'#5E63DA'}
               content={contentfree}
             />
             <Plan
-              title="Pro"
+              title='Pro'
               svg={ProPlanA}
-              price={10}
+              price={'9.99'}
               mrp={12}
-              color1={"#F6C149"}
-              color2={"#EB5779"}
+              color1={'#F6C149'}
+              color2={'#EB5779'}
               content={contentPremimum}
             />
           </>
         ) : (
           <>
             <Plan
-              title="Advanced"
+              title='Advanced'
               svg={AdvancePlanA}
               price={60}
               mrp={120}
-              color1={"#FFD568"}
-              color2={"#FB993F"}
+              color1={'#FFD568'}
+              color2={'#FB993F'}
               content={contentPremimum}
               yearly={!toggle}
             />
             <Plan
-              title="Advanced"
+              title='Advanced'
               svg={AdvancePlanA}
               price={110}
               mrp={199}
-              color1={"#FFD568"}
-              color2={"#FB993F"}
+              color1={'#FFD568'}
+              color2={'#FB993F'}
               content={contentPremimum}
               yearly={!toggle}
             />
--- src/Components/PlansComponent.jsx
+++ src/Components/PlansComponent.jsx
@@ -1,38 +1,38 @@
-import React from "react";
-import Star1 from "../assets/Star1.svg";
-import Star2 from "../assets/Star2.svg";
-import yellowThunder from "../assets/yellowThunder.svg";
-import BlueFire from "../assets/BlueFire.svg";
-import Cloud from "../assets/Cloud.svg";
+import React from 'react';
+import Star1 from '../assets/Star1.svg';
+import Star2 from '../assets/Star2.svg';
+import yellowThunder from '../assets/yellowThunder.svg';
+import BlueFire from '../assets/BlueFire.svg';
+import Cloud from '../assets/Cloud.svg';
 
 function DynamicText({ icon, Heading, subHeading }) {
   return (
-    <div className="my-5  relative">
+    <div className='my-5  relative'>
       {/* absolute in */}
-      <div className="absolute top-0 left-0 -z-10 hidden md:flex">
-        <img src={Cloud} alt="" className="" />
+      <div className='absolute top-0 left-0 -z-10 hidden md:flex'>
+        <img src={Cloud} alt='' className='' />
       </div>
       {/* absolute out */}
       {/* 2nd absolute in */}
-      <div className="absolute top-0 right-0 z-[1] hidden md:flex ">
-        <img src={Cloud} alt="" className="" />
+      <div className='absolute top-0 right-0 z-[1] hidden md:flex '>
+        <img src={Cloud} alt='' className='' />
       </div>
       {/* 2nd absolute out */}
-      <div className="flex flex-row items-center justify-around space-y-5 ">
-        <img src={yellowThunder} alt="" className="hidden md:flex " />
-        <div className="flex flex-col items-center justify-center  ">
-          <div className="flex flex-row items-center justify-center space-x-4 ">
-            <img src={icon} alt="" />
-            <p className="font-subHeading text-simple">{Heading}</p>
+      <div className='flex flex-row items-center justify-around space-y-5 '>
+        <img src={yellowThunder} alt='' className='hidden md:flex ' />
+        <div className='flex flex-col items-center justify-center  '>
+          <div className='flex flex-row items-center justify-center space-x-4 '>
+            <img src={icon} alt='' />
+            <p className='font-subHeading text-simple'>{Heading}</p>
           </div>
-          <p className="font-heading text-heading text-center mt-2">
+          <p className='font-heading text-heading text-center mt-2'>
             {subHeading}
           </p>
         </div>
-        <img src={BlueFire} alt="" className="hidden md:flex z-[2]" />
+        <img src={BlueFire} alt='' className='hidden md:flex z-[2]' />
       </div>
-      <p className="text-subHeading font-subHeading text-center container md:w-[75%] w-[100%] md:mt-[-40px] ">
-        Your digital life protected from 69$ a month{" "}
+      <p className='text-subHeading font-subHeading text-center container md:w-[75%] w-[100%] md:mt-[-40px] '>
+        Your digital life protected from 9.99$ a month{' '}
       </p>
     </div>
   );
